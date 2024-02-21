429769ce5be94efc81be80a39a588dd8f94aca15
diegochine <chine.diego@gmail.com>
2024-02-21 16:05:34 +0100

    update compute_conf_int.py

--- src/utils/compute_conf_int.py
+++ src/utils/compute_conf_int.py
@@ -11,13 +11,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def get_from_wandb(run, set, use_final, use_deterministic, metrics, variant_key, store_keys):
+def get_from_wandb(run, set, metrics, variant_key, store_keys):
     """Download run data from wandb.
     :param run: wandb.Run
     :param set: str; either 'test' or 'valid'
-    :param use_final: bool; if True, consider the end-training policy, else consider the best policy. Defaults to False.
-    :param use_deterministic: bool; if True, consider deterministic evaluation, otherwise the stochastic one.
-        Defaults to False.
     :param metrics: tuple of str; the metrics to download.
     :param variant_key: str; the key to use to identify the variant;
         it must be a sequence of config keys separated by "." e.g. "environment.instances.train".
@@ -39,13 +36,17 @@ def get_from_wandb(run, set, use_final, use_deterministic, metrics, variant_key,
         return cfg
 
     hyperparams = {k: unwrap_config(k) for k in [variant_key, *store_keys]}
-    prefix = f'{set}/{"final" if use_final or run.config["agent"]["algo"] == "ppo" else "best"}/{"deterministic" if use_deterministic else "stochastic"}'
-    keys = [f'{prefix}/{m}' for m in metrics]
-    h = run.history(samples=1, keys=keys, pandas=False)
 
+    metrics_dict = dict()
+    for v1 in ('final', 'best'):
+        for v2 in ('deterministic', 'stochastic'):
+            keys = [f'{set}/{v1}/{v2}/{m}' for m in metrics]
+            h = run.history(samples=1, keys=keys, pandas=False)
+            del h[0]['_step']
+            metrics_dict.update(h[0])
     return {**hyperparams,
             'cost_limit': run.config['agent']['lagrange']['params']['cost_limit'],
-            **{k.split('/')[-1]: h[0][k] for k in keys}}
+            **{k: v for k, v in metrics_dict.items()}}
 
 
 def get_args():
@@ -59,6 +60,7 @@ def get_args():
                         default=('avg_cost', 'avg_score', 'surrogate_score'))
     parser.add_argument('--variant-key', type=str, default='agent.algo')
     parser.add_argument('--store-keys', nargs='+', default=tuple())
+    parser.add_argument('--hue', type=lambda x: None if x in ('null', 'None') else x, default='null')
     return parser.parse_args()
 
 
@@ -90,7 +92,7 @@ def sanitize(variant_key, metric=None):
 if __name__ == '__main__':
     args = get_args()
     print('Running with args:\n', args)
-    save_path = f"cis/{'-'.join(args.sweep_id)}"
+    save_path = f"analysis/{'-'.join(args.sweep_id)}"
     if not os.path.isdir(save_path):
         os.makedirs(save_path)
 
@@ -99,10 +101,9 @@ if __name__ == '__main__':
 
         runs = [r for s_id in args.sweep_id for r in api.sweep(f'unify/long-term-constraints/{s_id}').runs]
 
-        map_f = partial(get_from_wandb, set=args.set, use_final=args.use_final,
-                        use_deterministic=args.use_deterministic, metrics=args.metrics,
+        map_f = partial(get_from_wandb, set=args.set, metrics=args.metrics,
                         variant_key=args.variant_key, store_keys=args.store_keys)
-        with ThreadPoolExecutor(max_workers=100) as ex:
+        with ThreadPoolExecutor(max_workers=25) as ex:
             print(f'Downloading data from wandb (Sweep ID: {args.sweep_id})')
             raw_data = ex.map(map_f, runs)
         df = pd.DataFrame(list(raw_data))
@@ -113,20 +114,37 @@ if __name__ == '__main__':
 
     variants = df[args.variant_key].unique()
     cost_limits = df['cost_limit'].unique()
-    cl_palette = dict(zip(cost_limits, sns.color_palette(n_colors=len(cost_limits))))
-    ys = {'avg_cost': 'Average cost', 'avg_score': 'Average return (normalized)',
-          'surrogate_score': 'Surrogate score'}
+    var_palette = dict(zip(variants, sns.color_palette(n_colors=len(variants))))
+    cl_palette = dict(zip(cost_limits, sns.color_palette(palette='Set2', n_colors=len(cost_limits))))
+    if args.hue is not None:
+        hue_vals = df[args.hue].unique()
+        assert len(hue_vals) > 1, f'There is only one unique val for {args.hue}, cannot use it as hue'
+        palette = dict(zip(hue_vals, sns.color_palette(palette='Set2' if args.hue == 'cost_limit' else 'deep', n_colors=len(hue_vals))))
+        plot_kwargs = {'palette': palette, 'hue': args.hue, 'dodge': 0.5}
+    else:
+        plot_kwargs = {}
 
-    for m in args.metrics:
-        cplot = sns.catplot(df, x=args.variant_key, y=m, hue='cost_limit', kind='bar', errorbar=lambda x: compute_ci(x),
-                            palette=cl_palette, alpha=0.5)
-        if m == 'avg_cost':
-            for cl in cost_limits:
-                plt.axhline(cl, linewidth=1, linestyle='--', color=cl_palette[cl], label=cl)
-        elif m == 'avg_score':
-            plt.ylim(0.85, 0.99)
-        cplot.fig.subplots_adjust(top=.95)
-        cplot.ax.set_title(f'{ys[m]} according to {sanitize(args.variant_key)}')
-        cplot.ax.set_xlabel(sanitize(args.variant_key))
-        cplot.ax.set_ylabel(ys[m])
-        cplot.fig.savefig(f'{save_path}/{sanitize(args.variant_key, m)}.png')
+    metrics_to_label = {'avg_cost': 'cost',
+                        'avg_score': 'return (normalized)',
+                        'surrogate_score': '$abs(J_C^{\\theta} - d)$'}
+    metrics_to_title = {'avg_cost': 'average cost',
+                        'avg_score': 'average return',
+                        'surrogate_score': 'constraint score'}
+
+    for v in ('deterministic', 'stochastic'):
+        for m in args.metrics:
+            cplot = sns.catplot(df, x=args.variant_key, y=f'{args.set}/best/{v}/{m}', **plot_kwargs, kind='point',
+                                errorbar=lambda x: compute_ci(x), linestyles='', capsize=.2, )
+            # sns.swarmplot(df, x=args.variant_key, y=f'{args.set}/best/{v}/{m}', hue='cost_limit', palette=cl_palette,
+            #               ax=cplot.ax, dodge=True, legend=False, size=5, edgecolor='black', linewidth=0.5, alpha=0.5)
+            if m == 'avg_cost':
+                for cl in cost_limits:
+                    plt.axhline(cl, linewidth=1, linestyle='--', color=cl_palette[cl], label=cl)
+                plt.ylim(0, 1100)
+            elif m == 'avg_score':
+                plt.ylim(0.85, 0.99)
+            cplot.fig.subplots_adjust(top=.95)
+            cplot.ax.set_title(f'95% CI for {metrics_to_title[m]} according to {sanitize(args.variant_key)}, {v} policy')
+            cplot.ax.set_xlabel(sanitize(args.variant_key))
+            cplot.ax.set_ylabel(metrics_to_label[m])
+            cplot.fig.savefig(f'{save_path}/{sanitize(args.variant_key, m)}-{v[:3]}.png', dpi=300)
